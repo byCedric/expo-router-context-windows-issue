166d4ecb1ba5a98feb4aed43551b8bd89148a215
Cedric van Putten <me@bycedric.com>
2022-09-29 00:35:48 +0200

    chore: add test files

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 // NOTE(EvanBacon): This file will go away in the future.
 import "@bacons/expo-metro-runtime";
-import { Text } from 'react-native';
 import { registerRootComponent } from "expo";
 import { ExpoRoot } from "expo-router";
 
 // Must be exported or Fast Refresh won't update the context
 export function App() {
   const ctx = require.context("./app");
-  console.log('INDEX', ctx.keys());
-  return <Text>Hello</Text>;
-  // return <ExpoRoot context={ctx} />;
+  return <ExpoRoot context={ctx} />;
 }
 
 registerRootComponent(App);
